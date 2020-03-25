ec706bcd93918c72cbde4e563637a5b09462a21c
kfyuta <yuta.funai@gmail.com>
2020-03-25 14:43:29 +0900

    Add incremental search

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,21 +16,20 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-
+  
   def edit
     @group = Group.find(params[:id])
   end
-
+  
   def update  
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      # binding.pry
       redirect_to group_messages_path(@group), notice: "グループを更新しました"
     else
       render :edit
     end
   end
-
+  
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
